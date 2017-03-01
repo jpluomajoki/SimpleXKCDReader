16be4a10a29bab11463145220acd79d66a2b3533
jpluomajoki <jpluomajoki@gmail.com>
2017-03-01 11:53:41 +0200

    Cleaning up and zoomable images.
    Cleaned up the code, commented it and made images usable using Chris
    Bane's Photoview.

--- app/src/main/java/com/example/jpluo/simplexkcdreader/MainActivity.java
+++ app/src/main/java/com/example/jpluo/simplexkcdreader/MainActivity.java
@@ -1,21 +1,16 @@
 package com.example.jpluo.simplexkcdreader;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.provider.ContactsContract;
-import android.support.annotation.IntegerRes;
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -24,47 +19,73 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
-    private static String XKCD_FRONTPAGE = "https://m.xkcd.com/";
-    private String xkcd_Url = XKCD_FRONTPAGE;
-    private String imageURL;
-    private EditText editText;
-    private int imageCounter = 0;
-    private Boolean changeSuccess;
-    private GestureDetectorCompat gestureDetector;
+    private PhotoView mPhotoView;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private static final String XKCD_FRONTPAGE = "https://m.xkcd.com/";
+    static int LAST_PAGE = -1;
+    private String mXkcd_url = XKCD_FRONTPAGE;
+    private String mImageURL;
+    private EditText mEditText;
+    private int mImageCounter = 0;
+    private Boolean mChangeSuccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-        editText = (EditText) findViewById(R.id.editText);
+        //setup the nextButton, editText and PhotoView
+        mPhotoView = (PhotoView) findViewById(R.id.photoView);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mNextButton = (ImageButton) findViewById(R.id.nextButton);
 
+        //Get the starting image and hide nextButton.
         updateImage();
-        Picasso.with(getBaseContext()).load(imageURL).into(imageView);
+        Picasso.with(getBaseContext()).load(mImageURL).into(mPhotoView);
+        LAST_PAGE = mImageCounter;
+        mNextButton.setVisibility(View.INVISIBLE);
 
-        final GestureDetector gdt = new GestureDetector(new GestureListener());
-        imageView.setOnTouchListener(new View.OnTouchListener() {
+        //setup listener for editText enter key.
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                gdt.onTouchEvent(event);
-                return true;
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+                mXkcd_url = XKCD_FRONTPAGE + mEditText.getText().toString();
+                updateImage();
+                return false;
+            }
+        });
+
+        //setup listener for the photoview swipe.
+        mPhotoView.setOnSingleFlingListener(new PhotoViewAttacher.OnSingleFlingListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                float x1 = e1.getX();
+                float x2 = e2.getX();
+
+                if ((x1 < x2) && (x2-x1 > 35.00)){
+                    on_prevButton_click(findViewById(R.id.prevButton));
+                } else if ((x2 < x1) && (x1 - x2 > 35.00)){
+                    on_nextButton_click(findViewById(R.id.nextButton));
+                }
+                return false;
             }
         });
     }
 
+
     public void updateImage(){
         try {
+            //Go to asynctask for getting image url, and wait for up to 5 seconds
             new GetImageUrl().execute().get(5000, TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -73,24 +94,38 @@ public class MainActivity extends AppCompatActivity {
         } catch (TimeoutException e) {
             e.printStackTrace();
         }
-        if (changeSuccess) {
-            Picasso.with(getBaseContext()).load(imageURL).into(imageView);
-            editText.setText(Integer.toString(imageCounter));
+        if (mChangeSuccess) {
+            //if getting image was successful, load the image onto Image view.
+            Picasso.with(getBaseContext()).load(mImageURL).into(mPhotoView);
+            //and set Edit text to match current page.
+            mEditText.setText(Integer.toString(mImageCounter));
+        }
+
+        //if on newest page, hide the next button.
+        if (mImageCounter == LAST_PAGE) {
+            mNextButton.setVisibility(View.INVISIBLE);
+        } else {
+            mNextButton.setVisibility(View.VISIBLE);
         }
     }
 
+
     public void on_prevButton_click(View view) {
-        xkcd_Url = XKCD_FRONTPAGE + Integer.toString(--imageCounter);
+        //On previous button click, update the url for the next page.
+        mXkcd_url = XKCD_FRONTPAGE + Integer.toString(--mImageCounter);
         updateImage();
-        Log.d("View: ", view.toString());
-        if (!changeSuccess) imageCounter++;
+
+        //if getting the url failed, don't change the image counter.
+        if (!mChangeSuccess) mImageCounter++;
     }
 
     public void on_nextButton_click(View view) {
-        xkcd_Url = XKCD_FRONTPAGE + Integer.toString(++imageCounter);
+        //On next button click, update the url for the next page.
+        mXkcd_url = XKCD_FRONTPAGE + Integer.toString(++mImageCounter);
         updateImage();
 
-        if (!changeSuccess) imageCounter--;
+        //if getting the url failed, don't change the image counter.
+        if (!mChangeSuccess) mImageCounter--;
     }
 
     private class GetImageUrl extends AsyncTask<Void, Void, Void> {
@@ -98,35 +133,25 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                Document doc = Jsoup.connect(xkcd_Url).get();
+                //Parse the HTML for image's url
+                Document doc = Jsoup.connect(mXkcd_url).get();
                 Element image = doc.select("img").first();
-                imageURL = image.absUrl("src");
+                mImageURL = image.absUrl("src");
+                //Now the image url is something like this: "/1805/". We must remove the first char
                 String imageNumberNoFirst = doc.select("a").get(1).attr("href").substring(1);
-                imageCounter = Integer.parseInt(imageNumberNoFirst.substring(0, imageNumberNoFirst.length()-1)) + 1;
-                Log.d("imageNum", Integer.toString(imageCounter));
-                Log.d("URL", imageURL);
-                changeSuccess = true;
+                //and the last char.
+                mImageCounter = Integer.parseInt(imageNumberNoFirst.substring(0, imageNumberNoFirst.length()-1)) + 1;
+                //The image url fetching was a success.
+                mChangeSuccess = true;
             } catch (IOException e) {
+                //Couldn't fetch image url. No internet or incorrect page number.
+                //TODO: print error to user (via toast?)
                 e.printStackTrace();
-                changeSuccess = false;
+                mChangeSuccess = false;
             }
             return null;
         }
     }
 
-
-    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
-            float x1 = e1.getX();
-            float x2 = e2.getX();
-            if (x1 < x2){
-                on_prevButton_click(findViewById(R.id.prevButton));
-            } else{
-                on_nextButton_click(findViewById(R.id.nextButton));
-            }
-            return false;
-        }
-    }
 }
+
